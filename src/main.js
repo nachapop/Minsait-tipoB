dc2999e318f867836db5e470e2a49b1dc0cb668f
nacho <ignaciogomez.contacto@gmail.com>
2019-02-15 17:48:44 +0000

    [fix] eslint nueva configuración

--- src/main.js
+++ src/main.js
@@ -1,23 +1,16 @@
-import Vue from 'vue';
-import axios from 'axios';
-import {
-  library,
-} from '@fortawesome/fontawesome-svg-core';
-import {
-  faAngleDown,
-  faAngleRight,
-} from '@fortawesome/free-solid-svg-icons';
-import {
-  FontAwesomeIcon,
-} from '@fortawesome/vue-fontawesome';
-import App from './App.vue';
+import Vue from "vue"
+import axios from "axios"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { faAngleDown, faAngleRight } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+import App from "./App.vue"
 
-library.add(faAngleDown);
-library.add(faAngleRight);
-Vue.component('font-awesome-icon', FontAwesomeIcon);
-Vue.config.productionTip = false;
+library.add(faAngleDown)
+library.add(faAngleRight)
+Vue.component("font-awesome-icon", FontAwesomeIcon)
+Vue.config.productionTip = false
 
-Vue.use(axios);
+Vue.use(axios)
 new Vue({
-  render: h => h(App),
-}).$mount('#app');
+  render: h => h(App)
+}).$mount("#app")
